267f56a284d9d47622b6bf153337449e7906b16a
WindOwOs 2.0 <swagat.ks.97@gmail.com>
2021-08-26 16:01:58 -0700

    added video/stream folders

---
+++ animations/00-sandbox.py
@@ -0,0 +1,12 @@
+from manim import *
+
+class SquareToCircle(Scene):
+    def construct(self):
+        square = Square()
+        circle = Circle()
+        circle.set_fill(PINK,opacity=0.5)
+        
+        self.play(Create(square))
+        self.play(Transform(square,circle))
+        self.wait(0.2)
+
